80964814ba98b3c2ec7537f4256fecd4d5ec501d
knnthlstrdgzmn <70429252+knnthlstrdgzmn@users.noreply.github.com>
2020-08-29 17:04:56 +0800

    Add files via upload

---
+++ parpuf 2.0/Updatefunc.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("myMidnav");
+    if (x.className === "midnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "midnav";
+    }
+  }
